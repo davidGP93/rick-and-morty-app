c214f9766d8aa2481418892e76997516efa990f1
davidGP93 <113630459+davidGP93@users.noreply.github.com>
2022-12-05 21:37:40 -0500

    feature(detailCharacter): remove console log

--- src/pages/Home/DetailCharacter/DetailCharacter.jsx
+++ src/pages/Home/DetailCharacter/DetailCharacter.jsx
@@ -10,7 +10,6 @@ export default function DetailCharacter() {
   useEffect(() => {
     getCharacterInfo(characterId).then((response) => {
       setCharacterInfo([response]);
-      console.log(response);
     });
   }, [characterId]);
 
